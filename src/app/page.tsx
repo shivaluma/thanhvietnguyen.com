2680b914327cc8cd2be928ec840bdb1744f909af
thanh.nguyen13 <thanh.nguyen13@be.com.vn>
2024-05-13 01:01:26 +0700

    chore: update header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,50 @@
 export default function Home() {
   return (
-    <main className="bg-slate-900 leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900">
-      thanh dep zai
-    </main>
+    <div className="lg:flex lg:justify-between lg:gap-4">
+      <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
+        <div>
+          <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">
+            <a href="/">Thanh Viet Nguyen</a>
+          </h1>
+          <h2 className="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">
+            Frontend Engineer
+          </h2>
+          <p className="mt-4 max-w-xs leading-normal">
+            I build pixel-perfect, engaging, and accessible digital experiences.
+          </p>
+          <nav className="nav hidden lg:block" aria-label="In-page jump links">
+            <ul className="mt-16 w-max">
+              <li>
+                <a
+                  className="group flex items-center py-3 active"
+                  href="#about"
+                >
+                  <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
+                  <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
+                    About
+                  </span>
+                </a>
+              </li>
+              <li>
+                <a className="group flex items-center py-3 " href="#experience">
+                  <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
+                  <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
+                    Experience
+                  </span>
+                </a>
+              </li>
+              <li>
+                <a className="group flex items-center py-3 " href="#projects">
+                  <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
+                  <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
+                    Projects
+                  </span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </div>
   );
 }
