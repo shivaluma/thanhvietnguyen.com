19a6027679306e00b7352c2e19214fe79e03bebc
thanh.nguyen13 <thanh.nguyen13@be.com.vn>
2024-05-23 15:59:20 +0700

    chore: test deeplink

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,6 +40,15 @@ export default function Home() {
                     Projects
                   </span>
                 </a>
+
+                <a href="xyz.be.customer://ride/home">test deeplink1</a>
+                <button
+                  onClick={() => {
+                    window.location.href = "xyz.be.customer://ride/home";
+                  }}
+                >
+                  test deeplink2
+                </button>
               </li>
             </ul>
           </nav>
